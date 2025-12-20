94e398ee83852f4abea3223242e200046307de8c
User <user@example.com>
2025-12-20 18:41:13 +0100

    Update configuration: Add start script to package.json and set server/preview host to '0.0.0.0' in vite.config.js for improved accessibility.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
     open: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000
   },
   build: {
     // Optimisation du build avec code splitting
